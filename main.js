1d8da121fbefb3a1d71903ef95dfa6da20211511
maya diretti <maya.diretti@icloud.com>
2023-11-10 13:28:29 +0100

    esercizio 1

---
+++ main.js
@@ -0,0 +1,14 @@
+/*esercizio 1*/ 
+window.addEventListener('load', () => {
+
+    const userInput = document.getElementById('testoUtente');
+    userInput.addEventListener('input', (e)=>{
+        const testo = e.target.value;
+
+    });
+});
+
+
+/*PURTROPPO NON SONO RIUSCITA A SVOLGERLI 
+PROVVEDERO A RIVEDERE LE LEZIONI E FARE
+ I VECCHI ESERCIZI NEL WEEKEND  */
